0f1c52c6cc537c88ace135cb37090a0b9e3922c0
Aditya <129072832+MIKEPENCE10CENT@users.noreply.github.com>
2025-01-13 21:20:31 +0530

    Update clkgen_200kHz.v

---
+++ Temp_Sensor_I2C_NexysA7/clkgen_200kHz.v
@@ -0,0 +1,42 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Aditya Soni 
+// 
+// Create Date: 13.10.2024 13:17:12
+// Design Name: 
+// Module Name: clock
+// Project Name: Temp Sensor 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+//ADITYA (2210993506)
+module clkgen_200kHz(
+    input clk_100MHz,
+    output clk_200kHz
+    );
+    
+    // 100 x 10^6 / 200 x 10^3 / 2 = 250 <-- 8 bit counter
+    reg [7:0] counter = 8'h00;
+    reg clk_reg = 1'b1;
+    
+    always @(posedge clk_100MHz) begin
+        if(counter == 249) begin
+            counter <= 8'h00;
+            clk_reg <= ~clk_reg;
+        end
+        else
+            counter <= counter + 1;
+    end
+    
+    assign clk_200kHz = clk_reg;
+    
+endmodule
